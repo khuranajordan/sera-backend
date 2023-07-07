d52d22ba4bfda815667b1f136ebb9f63ddd0bb11
ArthurLawrence-FahadAli <fahad.ali1@arthurlawrence.net>
2023-07-07 06:22:03 -0500

    Remove Huskey and Update Prettier

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,80 +1,83 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
-const { toJSON, paginate } = require('./plugins');
-const { roles } = require('../config/roles');
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const { toJSON, paginate } = require('./plugins')
+const { roles } = require('../config/roles')
 
 const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error('Invalid email');
-        }
-      },
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 8,
-      validate(value) {
-        if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-          throw new Error('Password must contain at least one letter and one number');
-        }
-      },
-      private: true, // used by the toJSON plugin
-    },
-
-    mobile: {
-      type: String,
-      required: true,
-      trim: true,
-      validate: {
-        validator: (value) => {
-          return /^[0-9]{10}$/.test(value);
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
         },
-        message: 'Invalid mobile number. Mobile number must contain exactly 10 digits.',
-      },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Invalid email')
+                }
+            },
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 8,
+            validate(value) {
+                if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+                    throw new Error(
+                        'Password must contain at least one letter and one number'
+                    )
+                }
+            },
+            private: true, // used by the toJSON plugin
+        },
+
+        mobile: {
+            type: String,
+            required: true,
+            trim: true,
+            validate: {
+                validator: (value) => {
+                    return /^[0-9]{10}$/.test(value)
+                },
+                message:
+                    'Invalid mobile number. Mobile number must contain exactly 10 digits.',
+            },
+        },
+        confirmPassword: {
+            type: String,
+            // required: true,
+            trim: true,
+            validate(value) {
+                if (value !== this.password) {
+                    throw new Error('Passwords do not match')
+                }
+            },
+        },
+        role: {
+            type: String,
+            enum: roles,
+            default: 'user',
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
     },
-    confirmPassword: {
-      type: String,
-      // required: true,
-      trim: true,
-      validate(value) {
-        if (value !== this.password) {
-          throw new Error('Passwords do not match');
-        }
-      },
-    },
-    role: {
-      type: String,
-      enum: roles,
-      default: 'user',
-    },
-    isEmailVerified: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+    {
+        timestamps: true,
+    }
+)
 
 // add plugin that converts mongoose to json
-userSchema.plugin(toJSON);
-userSchema.plugin(paginate);
+userSchema.plugin(toJSON)
+userSchema.plugin(paginate)
 
 /**
  * Check if email is taken
@@ -83,9 +86,9 @@ userSchema.plugin(paginate);
  * @returns {Promise<boolean>}
  */
 userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
-  const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
-  return !!user;
-};
+    const user = await this.findOne({ email, _id: { $ne: excludeUserId } })
+    return !!user
+}
 
 /**
  * Check if password matches the user's password
@@ -93,21 +96,21 @@ userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
  * @returns {Promise<boolean>}
  */
 userSchema.methods.isPasswordMatch = async function (password) {
-  const user = this;
-  return bcrypt.compare(password, user.password);
-};
+    const user = this
+    return bcrypt.compare(password, user.password)
+}
 
 userSchema.pre('save', async function (next) {
-  const user = this;
-  if (user.isModified('password')) {
-    user.password = await bcrypt.hash(user.password, 8);
-  }
-  next();
-});
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
 
 /**
  * @typedef User
  */
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,11 +1,11 @@
-const jwt = require('jsonwebtoken');
-const moment = require('moment');
-const httpStatus = require('http-status');
-const config = require('../config/config');
-const userService = require('./user.service');
-const { Token } = require('../models');
-const ApiError = require('../utils/ApiError');
-const { tokenTypes } = require('../config/tokens');
+const jwt = require('jsonwebtoken')
+const moment = require('moment')
+const httpStatus = require('http-status')
+const config = require('../config/config')
+const userService = require('./user.service')
+const { Token } = require('../models')
+const ApiError = require('../utils/ApiError')
+const { tokenTypes } = require('../config/tokens')
 
 /**
  * Generate token
@@ -16,14 +16,14 @@ const { tokenTypes } = require('../config/tokens');
  * @returns {string}
  */
 const generateToken = (userId, expires, type, secret = config.jwt.secret) => {
-  const payload = {
-    sub: userId,
-    iat: moment().unix(),
-    exp: expires.unix(),
-    type,
-  };
-  return jwt.sign(payload, secret);
-};
+    const payload = {
+        sub: userId,
+        iat: moment().unix(),
+        exp: expires.unix(),
+        type,
+    }
+    return jwt.sign(payload, secret)
+}
 
 /**
  * Save a token
@@ -35,15 +35,15 @@ const generateToken = (userId, expires, type, secret = config.jwt.secret) => {
  * @returns {Promise<Token>}
  */
 const saveToken = async (token, userId, expires, type, blacklisted = false) => {
-  const tokenDoc = await Token.create({
-    token,
-    user: userId,
-    expires: expires.toDate(),
-    type,
-    blacklisted,
-  });
-  return tokenDoc;
-};
+    const tokenDoc = await Token.create({
+        token,
+        user: userId,
+        expires: expires.toDate(),
+        type,
+        blacklisted,
+    })
+    return tokenDoc
+}
 
 /**
  * Verify token and return token doc (or throw an error if it is not valid)
@@ -52,13 +52,18 @@ const saveToken = async (token, userId, expires, type, blacklisted = false) => {
  * @returns {Promise<Token>}
  */
 const verifyToken = async (token, type) => {
-  const payload = jwt.verify(token, config.jwt.secret);
-  const tokenDoc = await Token.findOne({ token, type, user: payload.sub, blacklisted: false });
-  if (!tokenDoc) {
-    throw new Error('Token not found');
-  }
-  return tokenDoc;
-};
+    const payload = jwt.verify(token, config.jwt.secret)
+    const tokenDoc = await Token.findOne({
+        token,
+        type,
+        user: payload.sub,
+        blacklisted: false,
+    })
+    if (!tokenDoc) {
+        throw new Error('Token not found')
+    }
+    return tokenDoc
+}
 
 /**
  * Generate auth tokens
@@ -66,24 +71,43 @@ const verifyToken = async (token, type) => {
  * @returns {Promise<Object>}
  */
 const generateAuthTokens = async (user) => {
-  const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
-  const accessToken = generateToken(user.id, accessTokenExpires, tokenTypes.ACCESS);
+    const accessTokenExpires = moment().add(
+        config.jwt.accessExpirationMinutes,
+        'minutes'
+    )
+    const accessToken = generateToken(
+        user.id,
+        accessTokenExpires,
+        tokenTypes.ACCESS
+    )
 
-  const refreshTokenExpires = moment().add(config.jwt.refreshExpirationDays, 'days');
-  const refreshToken = generateToken(user.id, refreshTokenExpires, tokenTypes.REFRESH);
-  await saveToken(refreshToken, user.id, refreshTokenExpires, tokenTypes.REFRESH);
+    const refreshTokenExpires = moment().add(
+        config.jwt.refreshExpirationDays,
+        'days'
+    )
+    const refreshToken = generateToken(
+        user.id,
+        refreshTokenExpires,
+        tokenTypes.REFRESH
+    )
+    await saveToken(
+        refreshToken,
+        user.id,
+        refreshTokenExpires,
+        tokenTypes.REFRESH
+    )
 
-  return {
-    access: {
-      token: accessToken,
-      expires: accessTokenExpires.toDate(),
-    },
-    refresh: {
-      token: refreshToken,
-      expires: refreshTokenExpires.toDate(),
-    },
-  };
-};
+    return {
+        access: {
+            token: accessToken,
+            expires: accessTokenExpires.toDate(),
+        },
+        refresh: {
+            token: refreshToken,
+            expires: refreshTokenExpires.toDate(),
+        },
+    }
+}
 
 /**
  * Generate reset password token
@@ -91,15 +115,30 @@ const generateAuthTokens = async (user) => {
  * @returns {Promise<string>}
  */
 const generateResetPasswordToken = async (email) => {
-  const user = await userService.getUserByEmail(email);
-  if (!user) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'No users found with this email');
-  }
-  const expires = moment().add(config.jwt.resetPasswordExpirationMinutes, 'minutes');
-  const resetPasswordToken = generateToken(user.id, expires, tokenTypes.RESET_PASSWORD);
-  await saveToken(resetPasswordToken, user.id, expires, tokenTypes.RESET_PASSWORD);
-  return resetPasswordToken;
-};
+    const user = await userService.getUserByEmail(email)
+    if (!user) {
+        throw new ApiError(
+            httpStatus.NOT_FOUND,
+            'No users found with this email'
+        )
+    }
+    const expires = moment().add(
+        config.jwt.resetPasswordExpirationMinutes,
+        'minutes'
+    )
+    const resetPasswordToken = generateToken(
+        user.id,
+        expires,
+        tokenTypes.RESET_PASSWORD
+    )
+    await saveToken(
+        resetPasswordToken,
+        user.id,
+        expires,
+        tokenTypes.RESET_PASSWORD
+    )
+    return resetPasswordToken
+}
 
 /**
  * Generate verify email token
@@ -107,17 +146,24 @@ const generateResetPasswordToken = async (email) => {
  * @returns {Promise<string>}
  */
 const generateVerifyEmailToken = async (user) => {
-  const expires = moment().add(config.jwt.verifyEmailExpirationMinutes, 'minutes');
-  const verifyEmailToken = generateToken(user.id, expires, tokenTypes.VERIFY_EMAIL);
-  await saveToken(verifyEmailToken, user.id, expires, tokenTypes.VERIFY_EMAIL);
-  return verifyEmailToken;
-};
+    const expires = moment().add(
+        config.jwt.verifyEmailExpirationMinutes,
+        'minutes'
+    )
+    const verifyEmailToken = generateToken(
+        user.id,
+        expires,
+        tokenTypes.VERIFY_EMAIL
+    )
+    await saveToken(verifyEmailToken, user.id, expires, tokenTypes.VERIFY_EMAIL)
+    return verifyEmailToken
+}
 
 module.exports = {
-  generateToken,
-  saveToken,
-  verifyToken,
-  generateAuthTokens,
-  generateResetPasswordToken,
-  generateVerifyEmailToken,
-};
+    generateToken,
+    saveToken,
+    verifyToken,
+    generateAuthTokens,
+    generateResetPasswordToken,
+    generateVerifyEmailToken,
+}
