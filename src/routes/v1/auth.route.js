b22edce9ca967f7189160d9309be87cedd19f756
Abuzar Mirza <abuzar.mirza+1@easyeat.ai>
2023-07-08 10:41:49 +0530

    updated test

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -11,7 +11,7 @@ router.post(
     validate(authValidation.register),
     authController.register
 )
-router.get('/login', (req, res) => res.send('Update'))
+router.get('/login', (req, res) => res.send('Updated'))
 router.post('/login', validate(authValidation.login), authController.login)
 router.post('/logout', validate(authValidation.logout), authController.logout)
 router.post(
