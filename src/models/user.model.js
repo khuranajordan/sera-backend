83e2cf9e358705a3086f2e5e44fd863746bf6328
Abuzar Mirza <abdemirza@gmail.com>
2023-07-09 13:09:49 +0530

    removed email as optional

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,89 +1,88 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
-const { toJSON, paginate } = require('./plugins')
-const { roles } = require('../config/roles')
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const {toJSON, paginate} = require('./plugins');
+const {roles} = require('../config/roles');
 
 const userSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            lowercase: true,
-            validate(value) {
-                if (!validator.isEmail(value)) {
-                    throw new Error('Invalid email')
-                }
-            },
-        },
-        password: {
-            type: String,
-            required: true,
-            trim: true,
-            minlength: 8,
-            validate(value) {
-                if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-                    throw new Error(
-                        'Password must contain at least one letter and one number'
-                    )
-                }
-            },
-            private: true, // used by the toJSON plugin
-        },
-        passcode: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            default: generatePasscode,
-        },
-        mobile: {
-            type: String,
-            required: true,
-            trim: true,
-            validate: {
-                validator: (value) => {
-                    return /^[0-9]{10}$/.test(value)
-                },
-                message:
-                    'Invalid mobile number. Mobile number must contain exactly 10 digits.',
-            },
-        },
-        role: {
-            type: String,
-            enum: roles,
-            default: 'user',
-        },
-        isEmailVerified: {
-            type: Boolean,
-            default: false,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
-)
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Invalid email');
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      validate(value) {
+        if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+          throw new Error(
+            'Password must contain at least one letter and one number',
+          );
+        }
+      },
+      private: true, // used by the toJSON plugin
+    },
+    passcode: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      default: generatePasscode,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: value => {
+          return /^[0-9]{10}$/.test(value);
+        },
+        message:
+          'Invalid mobile number. Mobile number must contain exactly 10 digits.',
+      },
+    },
+    role: {
+      type: String,
+      enum: roles,
+      default: 'user',
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 // add plugin that converts mongoose to json
-userSchema.plugin(toJSON)
-userSchema.plugin(paginate)
+userSchema.plugin(toJSON);
+userSchema.plugin(paginate);
 
 function generatePasscode() {
-    const passcodeLength = 6
-    const digits = '0123456789'
-    let passcode = ''
-    for (let i = 0; i < passcodeLength; i++) {
-        const randomIndex = Math.floor(Math.random() * digits.length)
-        passcode += digits[randomIndex]
-    }
-    return passcode
+  const passcodeLength = 6;
+  const digits = '0123456789';
+  let passcode = '';
+  for (let i = 0; i < passcodeLength; i++) {
+    const randomIndex = Math.floor(Math.random() * digits.length);
+    passcode += digits[randomIndex];
+  }
+  return passcode;
 }
 
 /**
@@ -93,9 +92,9 @@ function generatePasscode() {
  * @returns {Promise<boolean>}
  */
 userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
-    const user = await this.findOne({ email, _id: { $ne: excludeUserId } })
-    return !!user
-}
+  const user = await this.findOne({email, _id: {$ne: excludeUserId}});
+  return !!user;
+};
 
 /**
  * Check if password matches the user's password
@@ -103,21 +102,21 @@ userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
  * @returns {Promise<boolean>}
  */
 userSchema.methods.isPasswordMatch = async function (password) {
-    const user = this
-    return bcrypt.compare(password, user.password)
-}
+  const user = this;
+  return bcrypt.compare(password, user.password);
+};
 
 userSchema.pre('save', async function (next) {
-    const user = this
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
-    }
-    next()
-})
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 
 /**
  * @typedef User
  */
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
