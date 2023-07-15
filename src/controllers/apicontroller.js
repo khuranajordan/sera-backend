b237c56f1319f1edd9d74ada96ef3058fc10c141
khuranarohan <rohankhurana@blod.in>
2023-07-15 10:50:58 +0530

    confirm password field added

--- src/controllers/apicontroller.js
+++ src/controllers/apicontroller.js
@@ -1,66 +1,83 @@
-const { json } = require('express');
+const {json} = require('express');
 const {User} = require('../models');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 // Define the decryptPass function
-const decryptPass = (encryptedPassword) => {
+const decryptPass = encryptedPassword => {
   // Implement your decryption logic here, for example using bcrypt
   // You can adjust this function to match the encryption method you are using
-  const decryptedPassword = bcrypt.compareSync('plainPassword', encryptedPassword);
+  const decryptedPassword = bcrypt.compareSync(
+    'plainPassword',
+    encryptedPassword,
+  );
   return decryptedPassword;
 };
 
-const create = async(req,res)=>{
-    try {
-        var user = await User.create(
-            {
-                name : req.body.name,
-                email:req.body.email,
-                password:req.body.password,
-                userisparent: req.body.userisparent,
-                mobile:req.body.mobile  
-            }
-        )
+const create = async (req, res) => {
+  try {
+    const {name, email, password, confirmPassword, userisparent, mobile} =
+      req.body;
 
-        var token = await jwt.sign(user.id, "abcdefghijklmn");
-      
-          user = user.toJSON();
-          user.token = token;
-          return res.status(200).json({ message: "create successfully", user });
-
-    } catch (error) {
-        console.log(error.message, "error");
-        return res.status(404).json(error.message);
-        
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !userisparent ||
+      !mobile
+    ) {
+      return res.status(400).json({message: 'All fields are required'});
     }
-}
+    // Check if password and confirmPassword match
+    if (password !== confirmPassword) {
+      return res.status(400).json({message: 'Passwords do not match'});
+    }
+
+    var user = await User.create({
+      name,
+      email,
+      password,
+      confirmPassword,
+      userisparent,
+      mobile,
+    });
+
+    var token = await jwt.sign(user.id, 'abcdefghijklmn');
+
+    user = user.toJSON();
+    user.token = token;
+    return res.status(200).json({message: 'User created successfully', user});
+  } catch (error) {
+    console.log(error.message, 'error');
+    return res.status(404).json(error.message);
+  }
+};
 
 const login = async (req, res) => {
-    try {
-      let { mobile, userisparent, password } = req.body;
-      var data = await User.findOne({ mobile });
-      if (!data) {
-        return res.status(202).json({ message: 'mobile number is not found' });
-      }
-      
-
-        if (!data || !(await data.isPasswordMatch(password))) {
-            throw new ApiError(httpStatus.UNAUTHORIZED, 'password does not match');
-          }
-      
-      let token = await jwt.sign(data.id, 'abcdefghijklmn');
-      token = token.token;
-      data = data.toJSON();
-      data.token = token;
-      return res.status(200).json({ message: 'login successfully', data });
-    } catch (error) {
-      console.log(error.message, 'in catch');
-      return res.status(400).json(error.message);
+  try {
+    let {mobile, userisparent, password} = req.body;
+    var data = await User.findOne({mobile});
+    if (!data) {
+      return res.status(202).json({message: 'mobile number is not found'});
     }
-  };
+
+    if (!data || !(await data.isPasswordMatch(password))) {
+      throw new ApiError(httpStatus.UNAUTHORIZED, 'password does not match');
+    }
+
+    let token = await jwt.sign(data.id, 'abcdefghijklmn');
+    token = token.token;
+    data = data.toJSON();
+    data.token = token;
+    return res.status(200).json({message: 'login successfully', data});
+  } catch (error) {
+    console.log(error.message, 'in catch');
+    return res.status(400).json(error.message);
+  }
+};
 
 module.exports = {
-create,
-login
-}
+  create,
+  login,
+};
--- src/routes/v1/apiroute.js
+++ src/routes/v1/apiroute.js
@@ -1,17 +1,14 @@
-const express = require('express')
-const auth = require('../../middlewares/auth')
-const validate = require('../../middlewares/validate')
-const userValidation = require('../../validations/user.validation')
-const userController = require('../../controllers/user.controller')
-const { create,
-        login
- } = require('../../controllers/apicontroller')
+const express = require('express');
+const auth = require('../../middlewares/auth');
+const validate = require('../../middlewares/validate');
+const userValidation = require('../../validations/user.validation');
+const userController = require('../../controllers/user.controller');
+const {create, login} = require('../../controllers/apicontroller');
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/create').post(create)
+router.route('/create').post(create);
 
-router.route('/login').post(login)
+router.route('/login').post(login);
 
-
-module.exports = router
+module.exports = router;
